67d35acfc8e341f5e95916ef7d6956840ba3dda0
Matthew Lee <30479162+fmmmlee@users.noreply.github.com>
2019-02-16 12:49:40 -0800

    Add files via upload
    Class that retrieves a JSON object from the Alpha Vantage API using the URL that GenerateURL creates.

---
+++ CallAPI.java
@@ -0,0 +1,38 @@
+//Author: Matthew Lee
+//In collaboration with Timothy Poehlman for WWU CS Hackathon 2019
+//TODO: Add Documentation
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Scanner;
+import org.json.*;
+
+public class CallAPI {
+
+	protected JSONObject getJSON(String inputURL) {
+		String data = null;
+		try {
+			//converting the string into something recognized as a URL
+			URL url = new URL(inputURL);
+			//opening a connection with the API using the URL
+			HttpURLConnection connection = (HttpURLConnection)url.openConnection();
+			connection.setRequestMethod("GET");
+			connection.connect();
+			//Scanning the JSON file at the URL
+			Scanner sc = new Scanner(url.openStream());
+			while(sc.hasNext()) {
+				//This is rather inefficient (copying the whole thing to a string), but I don't think it can be avoided.
+				data += sc.nextLine();
+			}
+			sc.close();
+		} catch (IOException E){
+			throw new RuntimeException("Invalid URL Format");
+		}
+		//If the program runs through the try-catch block without any issues, it makes it here. If it doesn't, well that's a problem with the code above.
+		//Either way I shouldn't have to worry about accidentally using null variables here; if they're null, it's because the above code didn't run, and that already would throw an exception
+		//and stop during runtime.
+		JSONObject convertedData = new JSONObject(data);
+		return convertedData;
+	}
+}
