6710c13c3f70e9c5eec1db2df4b9bd048588ad1a
Matthew Lee <30479162+fmmmlee@users.noreply.github.com>
2019-02-17 08:13:36 -0800

    Update CallAPI.java

--- CallAPI.java
+++ CallAPI.java
@@ -2,128 +2,41 @@
 //In collaboration with Timothy Poehlman and Griffin Nicolino for WWU CS Hackathon 2019
 //TODO: Add Documentation
 
-import java.util.Set;
-
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Scanner;
 import com.google.gson.*;
 
-//Today's High
-//Today's low
-//Yesterday ending
-//
-public class AnalyzeValues{
+public class CallAPI {
 	
-	static double estimateTomorrowClosing(JsonObject input, int time) {
-		double difference = 0;
-		String timePeriod;
-		if(time == 0) {
-			timePeriod = "Time Series (Daily)";
-		} else if (time == 1) {
-			timePeriod = "Weekly Time Series";
-		} else {
-			timePeriod = "Monthly Time Series";
-		}
-		JsonObject dailySeries = input.getAsJsonObject(timePeriod);
-		//from what I understand, due to the way the keySet() function is constructed, using the map interface, the resulting set preserves the order of entry, and it preserves it when
-		//converting to another ordered data structure as well.
-		Set<String> keys =  dailySeries.keySet();
-		String[] keysArray = (String[]) keys.toArray();
-		JsonObject today = dailySeries.getAsJsonObject(keysArray[0]);
-		//alternatively, simply retrieve this as a double. today.get("4. close").getAsDouble();
-		//I'm not sure if this .get(int).toString() business works, but I think it should, as Eclipse tends to warn me if something is out of place.
-		double todayNum = Double.valueOf(today.get("4. close").toString());
-		
-		
-		if(time == 0) {
-			for(int i = 0; i < 7; i++){
-				JsonObject day = dailySeries.getAsJsonObject(keysArray[i]);
-				JsonObject dayBefore = dailySeries.getAsJsonObject(keysArray[i+1]);
-				double dayNum = Double.valueOf(day.get("4. close").toString());
-				double afterNum = Double.valueOf(dayBefore.get("4. close").toString());
-				difference += (dayNum - afterNum);
+	public static JsonObject getJSON(String inputURL) {
+		String data = null;
+		try {
+			//converting the string into something recognized as a URL
+			URL url = new URL(inputURL);
+			//opening a connection with the API using the URL
+			HttpURLConnection connection = (HttpURLConnection)url.openConnection();
+			connection.setRequestMethod("GET");
+			connection.connect();
+			//Scanning the JSON file at the URL
+			Scanner sc = new Scanner(url.openStream());
+			while(sc.hasNext()) {
+				//This is rather inefficient (copying the whole thing to a string), but I don't think it can be avoided.
+				data += sc.nextLine();
 			}
-			difference /=7;
-		} else if(time == 2){
-			for(int i = 0; i < 12; i++){
-				JsonObject day = dailySeries.getAsJsonObject(keysArray[i]);
-				JsonObject dayBefore = dailySeries.getAsJsonObject(keysArray[i+1]);
-				double dayNum = Double.valueOf(day.get("4. close").toString());
-				double afterNum = Double.valueOf(dayBefore.get("4. close").toString());
-				difference += (dayNum - afterNum);
-			}
-			difference /=12;
-		} else {
-			for(int i = 0; i < 4; i++){
-				JsonObject day = dailySeries.getAsJsonObject(keysArray[i]);
-				JsonObject dayBefore = dailySeries.getAsJsonObject(keysArray[i+1]);
-				double dayNum = Double.valueOf(day.get("4. close").toString());
-				double afterNum = Double.valueOf(dayBefore.get("4. close").toString());
-				difference += (dayNum - afterNum);
-			}
-			difference /= 4;
+			sc.close();
+		} catch (IOException E){
+			throw new RuntimeException("Invalid URL Format");
 		}
 		
-		
-		
-		//returning today's closing value added to the average difference in closing over the past week
-		return (todayNum + difference);
+		//removing empty space at beginning of JSON that was tripping up the JSON conversion
+		int i = data.indexOf("{");
+		data = data.substring(i);
+		//If the program runs through the try-catch block without any issues, it makes it here. If it doesn't, well that's a problem with the code above.
+		//Either way I shouldn't have to worry about accidentally using null variables here; if they're null, it's because the above code didn't run, and that already would throw an exception
+		//and stop during runtime.
+		JsonObject convertedData = (new JsonParser()).parse(data.trim()).getAsJsonObject();
+		return convertedData;
 	}
-	
-	static String lastTradingDayStats(JsonObject input) {
-		//getting today's opening and closing values - same as the first part of the estimateTomorrowClosing() function
-		JsonObject dailySeries = input.getAsJsonObject("Time Series (Daily)");
-		Set<String> keys =  dailySeries.keySet();
-		String[] keysArray = (String[]) keys.toArray();
-		JsonObject today = dailySeries.getAsJsonObject(keysArray[0]);
-		
-		String open = today.get("1. open").toString();
-		String close = today.get("4. close").toString();
-		String high = today.get("2. high").toString();
-		String low = today.get("3. low").toString();
-		
-		return "Opening Price: " + open + " Closing Price: " + close + " High: " + high + " Low: " + low;
-	}
-	
-	static String intradayStats(JsonObject input) {
-		JsonObject currentSeries = input.getAsJsonObject("Time Series (1min)");
-		JsonArray arrayVersion = currentSeries.getAsJsonArray();
-		JsonObject currenttime = currentSeries.getAsJsonObject(arrayVersion.get(0).toString());
-		
-		String open = currenttime.get("1. open").toString();
-		String close = currenttime.get("4. close").toString();
-		String high = currenttime.get("2. high").toString();
-		String low = currenttime.get("3. low").toString();
-		
-		return "Opening Price: " + open + " Closing Price: " + close + " High: " + high + " Low: " + low;
-	}
-	
-	
-	static String foreignExchange(JsonObject input) {
-		JsonObject currentExchange = input.getAsJsonObject("Realtime Currency Exchange Rate");
-		String from = currentExchange.get("2. From_Currency Name").toString();
-		String to = currentExchange.get("4. To_Currency Name").toString();
-		String value = currentExchange.get("5. Exchange Rate").toString();
-		
-		return "A single " + from + " is equal to " + value + " " + to;
-	}
-	
-	/*
-	static void generateGraph(JsonObject input) {
-		int[] values = new int[365];
-		int[] indices = new int[365];
-		JsonObject dailySeries = input.getAsJsonObject("Time Series (Daily)");
-		JsonArray arrayVersion = dailySeries.getAsJsonArray();
-		for(int i = 0; i < 365; i++){
-			JsonObject day = dailySeries.getAsJsonObject(arrayVersion.get(i).toString());
-			double dayNum = Double.valueOf(day.get("4. close").toString());
-			//the graph library being used can't handle 
-			dayNum*=100;
-			values[i] = (int) dayNum;
-		}
-		for(int i = 0; i<365; i++) {
-			indices[i] = i;
-		}
-	}*/
 }
-
-
-
