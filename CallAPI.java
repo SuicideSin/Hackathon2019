7cae562abb6ec1d3b7c5cce36eec69750545641d
Matthew Lee <30479162+fmmmlee@users.noreply.github.com>
2019-02-17 00:16:05 -0800

    Update CallAPI.java

--- CallAPI.java
+++ CallAPI.java
@@ -1,16 +1,16 @@
 //Author: Matthew Lee
-//In collaboration with Timothy Poehlman for WWU CS Hackathon 2019
+//In collaboration with Timothy Poehlman and Griffin Nicolino for WWU CS Hackathon 2019
 //TODO: Add Documentation
 
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.Scanner;
-import org.json.*;
+import com.google.gson.*;
 
 public class CallAPI {
 	
-	public static JSONObject getJSON(String inputURL) {
+	public static JsonObject getJSON(String inputURL) {
 		String data = null;
 		try {
 			//converting the string into something recognized as a URL
@@ -36,7 +36,7 @@ public class CallAPI {
 		//If the program runs through the try-catch block without any issues, it makes it here. If it doesn't, well that's a problem with the code above.
 		//Either way I shouldn't have to worry about accidentally using null variables here; if they're null, it's because the above code didn't run, and that already would throw an exception
 		//and stop during runtime.
-		JSONObject convertedData = new JSONObject(data.trim());
+		JsonObject convertedData = (new JsonParser()).parse(data.trim()).getAsJsonObject();
 		return convertedData;
 	}
 }
